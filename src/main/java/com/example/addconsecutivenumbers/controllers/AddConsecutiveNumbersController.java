66c74c4bf1accd0305f2c40725cb9b90f6a1dd43
Ricardo <ricardo.test@test.com>
2024-02-21 02:09:38 -0600

    remove linear solution

--- src/main/java/com/example/addconsecutivenumbers/controllers/AddConsecutiveNumbersController.java
+++ src/main/java/com/example/addconsecutivenumbers/controllers/AddConsecutiveNumbersController.java
@@ -10,17 +10,8 @@ import java.util.stream.LongStream;
 @RestController
 public class AddConsecutiveNumbersController {
 
-    // O(n)
-    @GetMapping("api/v1/add-consecutive-numbers")
-    public ServiceResponse addConsecutiveNumbersLinear(@RequestParam("target") long target) {
-        long result = LongStream.rangeClosed(1, target)
-                .reduce(0, Long::sum);
-        return new ServiceResponse(result);
-    }
-
-    // O(1)
     @GetMapping("api/v2/add-consecutive-numbers")
-    public ServiceResponse addConsecutiveNumbersConstant(@RequestParam("target") long target) {
+    public ServiceResponse addConsecutiveNumbers(@RequestParam("target") long target) {
         long result = target * (target + 1) / 2;
         return new ServiceResponse(result);
     }
